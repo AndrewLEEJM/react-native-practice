8b7fdf7f838f0e94156079baf5c7f17b3c16182b
AndrewLEEJM <woals214@gmail.com>
2020-11-25 09:23:36 +0900

    todoapp

--- App.js
+++ App.js
@@ -6,7 +6,7 @@
  * @flow strict-local
  */
 
-import React, {useState} from 'react';
+import React, {Component, useState} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -27,151 +27,37 @@ import {
   DebugInstructions,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
-const yous = (one, two, three) => {
-  return one + ', ' + two + ', ' + three;
-};
-const PizzaWrite = () => {
-  const [textMy, setText] = useState('');
-  return (
-    <>
-      <TextInput
-        placeholder="Type here to translate!"
-        onChangeText={(txt) => setText(txt)}
-        defaultValue={textMy}
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      book: '리액트 배우기',
+    };
+  }
+  updateBook() {
+    this.setState({
+      book: '리액트 입문자',
+    });
+  }
+  render() {
+    return (
+      <BookDisplay
+        updateBook={() => this.updateBook()}
+        book={this.state.book}
       />
-      <Text>
-        {textMy
-          .split(' ')
-          .map((word) => word && '🍕')
-          .join(' ')}
-      </Text>
-    </>
-  );
-};
-const logo = {
-  uri: 'https://reactnative.dev/img/tiny_logo.png',
-  width: 64,
-  height: 64,
-};
+    );
+  }
+}
 
-const Scroll = () => (
-  <ScrollView>
-    <Text style={{fontSize: 96}}>Scroll me plz</Text>
-    <Image source={logo} />
-    <Image source={logo} />
-    <Image source={logo} />
-    <Image source={logo} />
-    <Image source={logo} />
-    <Text style={{fontSize: 96}}>If you like</Text>
-    <Image source={logo} />
-    <Image source={logo} />
-    <Image source={logo} />
-    <Image source={logo} />
-    <Image source={logo} />
-    <Text style={{fontSize: 96}}>Scrolling down</Text>
-    <Image source={logo} />
-    <Image source={logo} />
-    <Image source={logo} />
-    <Image source={logo} />
-    <Image source={logo} />
-    <Text style={{fontSize: 96}}>What's the best</Text>
-    <Image source={logo} />
-    <Image source={logo} />
-    <Image source={logo} />
-    <Image source={logo} />
-    <Image source={logo} />
-    <Text style={{fontSize: 96}}>Framework around?</Text>
-    <Image source={logo} />
-    <Image source={logo} />
-    <Image source={logo} />
-    <Image source={logo} />
-    <Image source={logo} />
-    <Text style={{fontSize: 80}}>React Native</Text>
-  </ScrollView>
-);
-const Help = (props) => {
-  const [isHungry, setIsHungry] = useState(true);
-  const [isNum, setIsNum] = useState(10);
-  return (
-    <View>
-      <Text>
-        I am {props.name}, and I am {isHungry ? 'hungry' : 'full'}!
-      </Text>
-      <Text>it's {isNum} O'clock!</Text>
-      <Button
-        onPress={() => {
-          setIsHungry(false);
-          setIsNum(12);
-        }}
-        disabled={!isHungry}
-        title={isHungry ? 'Pour me some milk, please!' : 'Thank you!'}
-      />
-    </View>
-  );
-};
-const App = () => {
-  const [date, setDate] = useState(new Date(1598051730000));
-  const [mode, setMode] = useState('date');
-  const [show, setShow] = useState(false);
-  const name = 'Noguri';
-  const onChange = (event, selectedDate) => {
-    const currentDate = selectedDate || date;
-    setShow(Platform.OS === 'ios');
-    setDate(currentDate);
-  };
-
-  const showMode = (currentMode) => {
-    setShow(true);
-    setMode(currentMode);
-  };
-
-  const showDatepicker = () => {
-    showMode('date');
-  };
-
-  const showTimepicker = () => {
-    showMode('time');
-  };
-  return (
-    <>
-      <View style={{alignItems: 'center'}}>
-        <Image
-          source={{uri: 'https://reactnative.dev/docs/assets/p_cat1.png'}}
-          style={{width: 200, height: 200}}
-        />
-      </View>
-      <Text>
-        Hello, I am {name} but also {yous('lion', 'snake', 'panda')}
-      </Text>
-      <TextInput
-        style={{height: 40, borderColor: 'gray', borderWidth: 1}}
-        defaultValue="Text me!!"
-      />
-      <Help name="승환" />
-      <Help name="동율" />
-      <PizzaWrite />
-      <Scroll />
+class BookDisplay extends Component {
+  render() {
+    return (
       <View>
-        <View>
-          <Button onPress={showDatepicker} title="Show date picker!" />
-        </View>
-        <View>
-          <Button onPress={showTimepicker} title="Show time picker!" />
-        </View>
-        {show && (
-          <DateTimePicker
-            testID="dateTimePicker"
-            timeZoneOffsetInMinutes={0}
-            value={date}
-            mode={mode}
-            is24Hour={true}
-            display="default"
-            onChange={onChange}
-          />
-        )}
+        <Text onPress={this.props.updateBook}>{this.props.book}</Text>
       </View>
-    </>
-  );
-};
+    );
+  }
+}
 
 export default App;
