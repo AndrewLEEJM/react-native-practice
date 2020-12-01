f4af7898d641207a2029126690af070dce463f58
AndrewLEEJM <woals214@gmail.com>
2020-12-01 22:17:27 +0900

    install immutability-helper

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './practice/profile-card/App';
+import App from './practice/profile-card/SmallProfileCard';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
